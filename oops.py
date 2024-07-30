88a0f103d4966ebfb895acaef314f6d1bcc9ad4f
Preethi <128659199+Preethi1205@users.noreply.github.com>
2024-07-30 19:25:12 +0530

    Day-6

---
+++ oops.py
@@ -0,0 +1,54 @@
+'''class Myclass:
+    #def __init__(self,a,b):
+     #   self.a=a
+      #  self.b=b
+    def add(a,b):
+        return a+b
+    def sub(a,b):
+        return a-b
+    def mul(a,b):
+        return a*b
+    def div(a,b):
+        return a/b
+    def mod(a,b):
+        return a%b
+o1=Myclass
+o2=Myclass
+a=int(input())
+b=int(input())
+print(o1.add(a,b))
+print(o1.sub(a,b))
+print(o2.add(a,b))
+print(o2.mul(a,b))
+
+'''
+
+'''class Myclass:
+    class_var="is a class variable"
+    def add(a,b):
+        ins_var="is instance variable"
+        print(ins_var)
+        return a+b
+    def sub(a,b):
+        return a-b
+    def mul(a,b):
+        return a*b
+    def div(a,b):
+        return a/b
+    def mod(a,b):
+        return a%b
+o1=Myclass
+o2=Myclass
+a=int(input())
+b=int(input())
+print(o1.add(a,b))
+print(o1.sub(a,b))
+print(o2.add(a,b))
+print(o2.mul(a,b))
+
+'''
+
+
+
+
+
